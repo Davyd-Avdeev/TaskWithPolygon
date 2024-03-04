e260d980a50480da08a4fb15bc830e3e67c3ce8a
avdeev.davyd@gmail.com <Avdeev>
2024-03-04 10:50:40 +0300

    Adding .dpr

---
+++ SecondTask/TaskWithBelongingToAFigure.dpr
@@ -0,0 +1,14 @@
+program TaskWithBelongingToAFigure;
+
+uses
+  Vcl.Forms,
+  SecondTask in 'SecondTask.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
